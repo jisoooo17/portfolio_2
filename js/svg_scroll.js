ebc6d6da89eadbff1b50e375f2a313c58ff7c4f6
jisoooo17 <jisoooo17@gmail.com>
2021-01-22 14:25:27 +0900

    origin

---
+++ js/svg_scroll.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    //초기 필요한 변수값 설정
+    var pos2 = $("#info ul li").offset().top;
+    var base = -600;
+
+    //브라우저 스크롤시
+    $(window).on("scroll", function(){
+        var scroll = $(this).scrollTop();
+
+        if( scroll >= pos2+base ){
+            scroll = (scroll-(pos2+base)) *10;
+
+            var result = 1780 - scroll;
+
+            if(result < 0 ) result = 0;  
+
+            var bg = scroll/6000;            
+
+        //    if(bg > 0.8) bg = 0.8;
+
+            $("#info path").css({ 
+                strokeDashoffset :  result,
+                 fill : "rgb(226, 171, 21,"+bg+")"
+                // fill : "rgba(0,0,0,"+bg+")"
+            });
+        }
+    })
+});
