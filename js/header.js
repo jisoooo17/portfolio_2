1d3c7ed8feba716b57a12a46136b4f0173714fec
jisoooo17 <jisoooo17@gmail.com>
2021-02-23 17:01:52 +0900

    skip

---
+++ js/header.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var $skip_a = $("#skipNavi a");
+
+    $skip_a.on("focusin",function(){
+        $(this).css({top:0});
+    });
+    $skip_a.on("focusout",function(){
+        $(this).css({top:-30});
+    })
+
+})
